60eaf2e56835aedf88d164a9cb105d60fd83c75f
chibimiku <dreamcosine@gmail.com>
2017-01-12 17:05:57 +0800

    add search.php
    oops why so soon

---
+++ search.php
@@ -0,0 +1,40 @@
+<?php 
+
+define ('IN_LED', true);
+
+require ('header.inc.php');
+require ('data/prolist.inc.php');
+
+?>
+
+<!-- Begin content-->
+<section id="content" class="clearfix">
+	<div id="search">
+		<div class="row">
+			<div class="span12"><h1>Search Results</h1></div>
+		</div>
+		<!-- Begin results -->
+		<div class="row results">
+			<div class="span1">
+				<div class="thumbnail">
+					<a href="/products/led-exit-sign-and-emergency-lighting"><img src="//cdn.shopify.com/s/files/1/0986/0548/products/9EC7D3AE702CE993B6EE8B4BA8733C3914B6126DC21D764068_pimgpsh_fullsize_distr_small.jpg?v=1464012898" alt="" /></a>
+				</div>
+			</div>
+			<div class="span11">
+				<div class="search-result">
+					<h3><a href="/products/led-exit-sign-and-emergency-lighting" title="">LED EXIT SIGN AND EMERGENCY LIGHTING</a></h3>
+					*Injecton-molded thermoplastic ABShousing,Retangular type plastic running man exit sign*UL 94V-0 flame rating*Single or double face configuration*Universal (wall,side or ceiling) mounting*120/347VAC dual voltage operation*1Wx2pcs emergency head*Low energy consuption only 2.0 watts at 120V*LED indicator light and push button <strong class="highlight">test</strong> switch*LEDs mounted...
+				</div>
+			</div>
+			<hr class="divider" />
+		</div>  
+		<!-- End results -->
+	</div> <!-- /#search -->
+</section>
+<!-- End content-->
+
+<?php
+
+require ('footer.inc.php');
+
+?>
