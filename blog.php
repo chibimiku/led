0e24c14c83269a4ef41ada9500bb15baf9fd946c
chibimiku <dreamcosine@gmail.com>
2017-01-12 15:13:23 +0800

    update blog.php

--- blog.php
+++ blog.php
@@ -8,46 +8,25 @@ require ('data/prolist.inc.php');
 ?>
 
 <!-- Begin content -->
-  <div class="row">
-    
-    
-    
-    <div class="span9 articles">
-      
-      <div class="article clearfix">
-        <h1><a href="/blogs/news/73960387-ccr-give-you-more-choice">CCR Give You More Choice</a> <span class="date" style="display: block;">Posted on 28 Oct 17:35 </span></h1>
-        <img src="//cdn.shopify.com/s/files/1/0986/0548/files/shilittu_2.jpg?8419443098483402060" alt="">
-      </div>
-      
-      
-      
-
-    </div>
-    
-    
-    
-    <!-- Begin sidebar -->
-    <div class="span3 sidebar">
-
-  <h3>Recent Articles</h3>
-  
-  <div class="article">
-    <p><a href="/blogs/news/73960387-ccr-give-you-more-choice">CCR Give You More Choice</a> <em>Posted on 28 Oct 17:35</em></p>
-  </div>
-  
-
-  
-
-</div>
-
-    <!-- End sidebar -->
-    
-  </div>
-  <!-- End content -->
-  
-</div>
-      </section>
-      <!-- End content-->
+<section id="content" class="clearfix">
+	<div class="row">
+		<div class="span9 articles">
+			<div class="article clearfix">
+				<h1><a href="/blogs/news/73960387-ccr-give-you-more-choice">CCR Give You More Choice</a> <span class="date" style="display: block;">Posted on 28 Oct 17:35 </span></h1>
+					<img src="//cdn.shopify.com/s/files/1/0986/0548/files/shilittu_2.jpg?8419443098483402060" alt="">
+			</div>
+		</div>
+		<!-- Begin sidebar -->
+		<div class="span3 sidebar">
+			<h3>Recent Articles</h3>
+			<div class="article">
+				<p><a href="/blogs/news/73960387-ccr-give-you-more-choice">CCR Give You More Choice</a> <em>Posted on 28 Oct 17:35</em></p>
+			</div>
+		</div>
+		<!-- End sidebar -->
+	</div>
+	<!-- End content -->
+</section>
 	  
 <?php
 require ('footer.inc.php');
