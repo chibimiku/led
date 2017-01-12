6fc1a665420893847e93bc91f1331ad79a87599d
chibimiku <dreamcosine@gmail.com>
2017-01-12 16:58:45 +0800

    fix footer
    fix images & some links

--- footer.inc.php
+++ footer.inc.php
@@ -17,7 +17,7 @@ if(!defined('IN_LED')){
 			<div class="span4">
 				<div class="p30">
 					<h4>Latest News</h4>
-					<p class="p10"><strong><a href="/blogs/news/73960387-ccr-give-you-more-choice" title="">CCR Give You More Choice</a></strong></p>
+					<p class="p10"><strong><a href="blog.php" title="">CCR Give You More Choice</a></strong></p>
 					<p></p>
 				</div>
 			</div>
@@ -26,8 +26,8 @@ if(!defined('IN_LED')){
 			<div class="span4 footer-menu">
 				<h4>Quick Links</h4>
 					<ul class="unstyled">
-						<li><a href="/search" title="Search">Search</a></li>
-						<li><a href="/pages/about-us" title="About Us">About Us</a></li>
+						<li><a href="search.php" title="Search">Search</a></li>
+						<li><a href="about.php" title="About Us">About Us</a></li>
 					</ul>
 			</div>
 			<!-- End footer navigation -->
@@ -39,11 +39,11 @@ if(!defined('IN_LED')){
 			<!-- Begin copyright -->
 			<div class="span12 tc copyright">
 				<ul class="credit-cards p10 clearfix">
-					<li><img src="//cdn.shopify.com/s/files/1/0986/0548/t/1/assets/icon-cc-visa.png?9800882074587372923" alt="Visa" /></li>
-					<li><img src="//cdn.shopify.com/s/files/1/0986/0548/t/1/assets/icon-cc-mastercard.png?9800882074587372923" alt="MasterCard" /></li>
-					<li><img src="//cdn.shopify.com/s/files/1/0986/0548/t/1/assets/icon-cc-amex.png?9800882074587372923" alt="Amex" /></li>
+					<li><img src="static/images/icon-cc-visa.png" alt="Visa" /></li>
+					<li><img src="static/images/icon-cc-mastercard.png" alt="MasterCard" /></li>
+					<li><img src="static/images/icon-cc-amex.png" alt="Amex" /></li>
 				</ul> <!-- /.credit-cards -->
-				<p>Copyright &copy; 2017 <a href="/" title="">CCR LED</a> | <a target="_blank" rel="nofollow" href="https://www.tsdm.me">Powered by chibimiku@TSDM</a></p>
+				<p>Copyright &copy; 2017 <a href="/" title="">CCR LED</a> | <a target="_blank" rel="nofollow" href="http://www.google.com">Powered by chibimiku@TSDM</a></p>
 			</div>
 			<!-- End copyright -->
       </div>
